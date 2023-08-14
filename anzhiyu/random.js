fb357f58062c05f9e4d685da5da10d6dae55b0eb
Chinzicam <a3189797316@163.com>
2023-08-14 11:13:29 +0800

    Site updated: 2023-08-14 11:13:29

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,1 +1,54 @@
-var posts=["2022/09/11/Git常用操作/","2022/10/06/Markdown-TyporaVSCode教程/","2023/05/15/Mybatis-Plus常用的查询方法/","2023/05/07/MybatisPlus快速上手/","2023/07/16/Redis基础篇/","2022/09/10/hexo/","2023/05/21/MyBatis-Plus公共字段自动填充/","2022/09/27/oracle初体验/","2022/10/31/骑砍二控制台教程/","2023/04/13/springboot整合mybatis/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};
+var posts=["2022/09/11/Git常用操作/","2022/10/06/Markdown-TyporaVSCode教程/","2023/05/21/MyBatis-Plus公共字段自动填充/","2023/05/15/Mybatis-Plus常用的查询方法/","2023/05/07/MybatisPlus快速上手/","2023/07/16/Redis基础篇/","2022/09/10/hexo/","2022/09/27/oracle初体验/","2023/04/13/springboot整合mybatis/","2022/10/31/骑砍二控制台教程/"];function toRandomPost(){pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);};var friend_link_list=[{"name":"Hexo","hundredSuffix":"","link":"https://hexo.io/zh-tw/","avatar":"https://d33wubrfki0l68.cloudfront.net/6657ba50e702d84afb32fe846bed54fba1a77add/827ae/logo.svg","descr":"快速、简单且强大的网站框架"},{"name":"anzhiyu主题","link":"https://blog.anheyu.com/","avatar":"https://img02.anheyu.com/adminuploads/1/2022/09/15/63232b7d91d22.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-blog@1.1.6/img/post/common/anzhiy.cn.jpg"},{"name":"安知鱼","hundredSuffix":"","link":"https://blog.anheyu.com/","avatar":"https://img02.anheyu.com/adminuploads/1/2022/09/15/63232b7d91d22.jpg","descr":"生活明朗，万物可爱","siteshot":"https://npm.elemecdn.com/anzhiyu-blog@1.1.6/img/post/common/anzhiy.cn.jpg"},{"name":"Moeyy","hundredSuffix":"","link":"https://moeyy.cn/","avatar":"https://i1.hdslb.com/bfs/face/94e387b6d660cf47b27999ba6b457bdb49ea25e2.jpg","descr":"一条有远大理想的咸鱼","siteshot":"https://pic.imgdb.cn/item/64d8cba41ddac507cc3ac8d7.png"},{"name":"安知鱼","hundredSuffix":"","link":"https://blog.anheyu.com","avatar":"https://img02.anheyu.com/adminuploads/1/2022/09/15/63232b7d91d22.jpg","descr":"生活明朗，万物可爱"}];
+    var refreshNum = 1;
+    function friendChainRandomTransmission() {
+      const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+      const { name, link } = friend_link_list.splice(randomIndex, 1)[0];
+      Snackbar.show({
+        text:
+          "点击前往按钮进入随机一个友链，不保证跳转网站的安全性和可用性。本次随机到的是本站友链：「" + name + "」",
+        duration: 8000,
+        pos: "top-center",
+        actionText: "前往",
+        onActionClick: function (element) {
+          element.style.opacity = 0;
+          window.open(link, "_blank");
+        },
+      });
+    }
+    function addFriendLinksInFooter() {
+      var footerRandomFriendsBtn = document.getElementById("footer-random-friends-btn");
+      if(!footerRandomFriendsBtn) return;
+      footerRandomFriendsBtn.style.opacity = "0.2";
+      footerRandomFriendsBtn.style.transitionDuration = "0.3s";
+      footerRandomFriendsBtn.style.transform = "rotate(" + 360 * refreshNum++ + "deg)";
+      const finalLinkList = [];
+  
+      let count = 0;
+
+      while (friend_link_list.length && count < 3) {
+        const randomIndex = Math.floor(Math.random() * friend_link_list.length);
+        const { name, link, avatar } = friend_link_list.splice(randomIndex, 1)[0];
+  
+        finalLinkList.push({
+          name,
+          link,
+          avatar,
+        });
+        count++;
+      }
+  
+      let html = finalLinkList
+        .map(({ name, link }) => {
+          const returnInfo = "<a class='footer-item' href='" + link + "' target='_blank' rel='noopener nofollow'>" + name + "</a>"
+          return returnInfo;
+        })
+        .join("");
+  
+      html += "<a class='footer-item' href='/link/'>更多</a>";
+
+      document.getElementById("friend-links-in-footer").innerHTML = html;
+
+      setTimeout(()=>{
+        footerRandomFriendsBtn.style.opacity = "1";
+      }, 300)
+    };
